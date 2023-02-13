d32adb3fcbe63a877caecdd006d92800ee79c5b9
mayud <mayudong200333@gmail.com>
2023-02-13 19:58:46 +0900

    added nextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '@/components/header'
+import {SessionProvider} from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps:{session,...pageProps} }: AppProps) {
   return (
-  <>
+  <SessionProvider session={session}>
     <Header/>
     <Component {...pageProps} />
-  </>
+  </SessionProvider>
   )
 }
